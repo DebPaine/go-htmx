0aab4e725c62dd72536d2ea62fb128e310e19ea9
unknown <debmalyapaine@gmail.com>
2024-06-01 00:39:59 +0530

    add: index.html file

--- main.go
+++ main.go
@@ -2,14 +2,37 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func main() {
 	fmt.Println("Hello World!")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello World!")
-	})
-	http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/", helloWorld)
+	http.HandleFunc("/home", helloWorldTemplate)
+
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func helloWorld(w http.ResponseWriter, r* http.Request){
+	fmt.Fprint(w, "Hello World!\n")
+	fmt.Fprint(w, r.Method)
+}
+
+func helloWorldTemplate(w http.ResponseWriter, r* http.Request){
+	templ, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = templ.Execute(w, nil)
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
